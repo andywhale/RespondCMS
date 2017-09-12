f4b3b15a9e3520dfc7f17b9b641bfde4f3ec90fc
vagrant <andy@awhale.co.uk>
2017-09-12 12:25:16 +0000

    Remove home link from dashboard

--- resources/views/admin/partials/nav.blade.php
+++ resources/views/admin/partials/nav.blade.php
@@ -1,14 +1,11 @@
 <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-  <a class="navbar-brand" href="#">Dashboard</a>
+  <a class="navbar-brand" href="\admin">Dashboard</a>
   <button class="navbar-toggler d-lg-none" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
 
   <div class="collapse navbar-collapse" id="navbarsExampleDefault">
     <ul class="navbar-nav mr-auto">
-      <li class="nav-item {{{ (Request::is('admin')) ? 'active' : '' }}}">
-        <a class="nav-link" href="\admin">Home</a>
-      </li>
       <li class="nav-item {{{ (Request::is('admin/content*')) ? 'active' : '' }}}">
         <a class="nav-link" href="\admin\content">Content</a>
       </li>
